70bb7e1bb054b2322f2d8d83c69deb72b51b43f0
GreedNeSS <ruslan199126@gmail.com>
2021-12-10 22:17:40 +0300

    create_template

---
+++ Cube/Cube.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+
+    class Cube
+    {
+        int x;
+        int y;
+        int z;
+
+        public int Width
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Height
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public int Lengthwise
+        {
+            get { return z; }
+            set { z = value; }
+        }
+
+        public int ShowVolume()
+        {
+            return x * y * z;
+        }
+
+        public void ExpandSize(int a, int b, int c)
+        {
+            /*
+              x += a;
+              y += b;
+              z += c;
+            */
+
+            Width += a;
+            Height += b;
+            Lengthwise += c;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -21,49 +21,6 @@ namespace Class
             Console.WriteLine("Extended cube volume: " + cube.ShowVolume());
 
             Console.ReadLine();
-        }               
-    }                   
-
-    class Cube
-    {
-        int x;
-        int y;
-        int z;
-
-        public int Width
-        {
-            get { return x; }
-            set { x = value; }
-        }
-
-        public int Height
-        {
-            get { return y; }
-            set { y = value; }
-        }
-
-        public int Lengthwise
-        {
-            get { return z; }
-            set { z = value; }
-        }
-
-        public int ShowVolume()
-        {
-            return x * y * z;
-        }
-
-        public void ExpandSize(int a, int b, int c)
-        {
-            /*
-              x += a;
-              y += b;
-              z += c;
-            */
-
-            Width += a;
-            Height += b;
-            Lengthwise += c;
         }
     }
 }
